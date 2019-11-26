134b2310f4db72a3412ca4653530eb1d50a1b4cf
Miha Svečak <36370648+svecko@users.noreply.github.com>
2019-11-26 13:54:05 +0100

    Updated Timer.h

--- Timer.h
+++ Timer.h
@@ -1,27 +1,17 @@
-#include "Timer.h"
+#pragma once
 
-Timer::Timer(const char* name)
-	: m_Name(name), m_Stopped(false)
+#include <chrono>
+
+class Timer
 {
-	m_StartTimepoint = std::chrono::high_resolution_clock::now();
-}
+public:
+	Timer(const char* name);
+	~Timer();
 
-Timer::~Timer()
-{
-	if (!m_Stopped)
-	{
-		Stop();
-	}
-}
+	void Stop();
 
-void Timer::Stop()
-{
-	auto endTimepoint = std::chrono::high_resolution_clock::now();
-
-	long long start = std::chrono::time_point_cast<std::chrono::nanoseconds>(m_StartTimepoint).time_since_epoch().count();
-	long long end = std::chrono::time_point_cast<std::chrono::nanoseconds>(endTimepoint).time_since_epoch().count();
-
-	std::cout << m_Name << ": " << (end - start) << "ns\n";
-
-	m_Stopped = true;
-}
+private:
+	const char* m_Name;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+	bool m_Stopped;
+};
